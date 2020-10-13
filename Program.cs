8d407f331d30b65f91d66df38c583197b86d1010
Sylvain-Decombe <sylvain.decombe@etu.unilim.fr>
2020-10-13 10:19:49 +0200

    Ajout Calculatrice et liste des commandes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace FilPasRouge
 {
@@ -6,19 +7,56 @@ namespace FilPasRouge
 	{
 		static void Main(string[] args)
 		{
+
+			string[] tabCommandes = {"Hello", "Add", "Sub", "Mul", "Div"};
+
 			if (args.Length < 1){
-				Console.Write("Ceci est une aide");
-			}else{
-				PremiereCommandes(args[0]);
+				Console.Write("Liste des commandes disponnibles : \n\n");
+				for (int i = 0; i < tabCommandes.Length; i++)
+				{
+					Console.Write(tabCommandes[i] + "\n");
+				}				
+			}
+			else
+			{
+				switch (args[0])
+				{
+					case "Hello" : 
+						Console.Write("Hello World !");
+						break;
+					case "Add" : 
+					case "Sub" :
+					case "Mul" :
+					case "Div" :
+						Calcul(args[0], float.Parse(args[1], CultureInfo.InvariantCulture), float.Parse(args[2], CultureInfo.InvariantCulture));
+						break;
+					default: 
+						Console.Write("Commande inconnue");
+						break;
+				}
 			}
 		}
 
-		public static void PremiereCommandes(string val){
+		public static void Calcul(string val, float nombre1, float nombre2){
 
+			float result = 0;
 			switch (val)
 			{
-				case "Hello" : 
-					Console.Write("Hello World !");
+				case "Add" : 
+					result = nombre1 + nombre2;
+					Console.Write("Resultat = " + result);
+					break;
+				case "Sub" : 
+					result = nombre1 - nombre2;
+					Console.Write("Resultat = " + result);
+					break;
+				case "Mul" : 
+					result = nombre1 * nombre2;
+					Console.Write("Resultat = " + result);
+					break;
+				case "Div" : 
+					result = nombre1 / nombre2;
+					Console.Write("Resultat = " + result);
 					break;				
 				default: 
 					Console.Write("Commande inconnue");
