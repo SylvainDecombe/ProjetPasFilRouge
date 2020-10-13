0d7849823ff653992754b956d1b240721b6cddb5
Sylvain-Decombe <sylvain.decombe@etu.unilim.fr>
2020-10-13 14:15:23 +0200

    Génération de la liste d'actions à l'aide de la récupération des classes qui héritent de IAction

--- Program.cs
+++ Program.cs
@@ -1,37 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 
 namespace FilPasRouge {
 	class Program {
 		static void Main (string[] args) {
 
-			List<IAction> availableActions = new List<IAction> ()
-			{
-				new Hello (),
-				new Add (),
-				new Sub (),
-				new Mul (),
-				new Div ()
-			};
+			Type actionType = typeof(IAction);
 
+			IEnumerable<Type> types = AppDomain.CurrentDomain.GetAssemblies ()
+				.SelectMany (s => s.GetTypes ())
+				.Where (p => actionType.IsAssignableFrom (p));
+
+			IAction[] availableActions = types
+				.Where(t => !t.IsInterface)
+				.Select (type => (IAction) Activator
+				.CreateInstance (type))
+				.ToArray ();
+			
 			if (args.Length < 1) {
 				Console.Write ("Liste des commandes disponnibles : \n\n");
-				foreach (var item in availableActions)
-				{
-					Console.Write(" ► " + item.Description + "\n");
+				foreach (var item in availableActions) {
+					Console.Write (" ► " + item.Description + "\n");
 				}
 			} 
 			else {
-				if (!availableActions.Exists(x => x.Name == args[0]))
-				{
-					Console.Write("Commande inconnue.");
-				}
-				else
-				{
-					availableActions.Find(x => x.Name == args[0]).Action(args);
+				if (!availableActions.Any (x => x.Name == args[0])) {
+					Console.Write ("Commande inconnue.");
+				} else {
+					availableActions.Where(x => x.Name == args[0]).FirstOrDefault().Action(args);
 				}
 			}
-		}		
+		}
 	}
 }
